c22089aa76f7001f88548fac3b9ff4b673cd8a3b
ThangND <thang.nguyen@netko-solution.com>
2024-11-06 13:54:53 +0700

    fix detect brower

--- src/components/UserProfile/Security.js
+++ src/components/UserProfile/Security.js
@@ -254,29 +254,35 @@ export default function Security({ isSignIn, step }) {
       )}
       <div className={styles.ox_row_end}>
         <div className={styles.ox_row_left}>Active devices</div>
-        <div className={styles.ox_row_right}>Chrome 127.0.0.0</div>
+        <div className={styles.ox_row_right}>{detectBrower()}</div>
       </div>
     </Fragment>
   )
 }
+function detectBrower() {
+  let text = "unknown"
+  try {
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR")) !== -1) {
+      text = "Opera"
+    } else if (navigator.userAgent.indexOf("Edg") !== -1) {
+      text = "Edge"
+    } else if (navigator.userAgent.indexOf("Chrome") !== -1) {
+      text = "Chrome"
+    } else if (navigator.userAgent.indexOf("Safari") !== -1) {
+      text = "Safari"
+    } else if (navigator.userAgent.indexOf("Firefox") !== -1) {
+      text = "Firefox"
+    } else if (navigator.userAgent.indexOf("MSIE") !== -1 || !!document.documentMode === true) {
+      //IF IE > 10
+      text = "IE"
+    } else {
+      text = "unknown"
+    }
+  } catch (error) {}
 
-const icKey = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    width={14}
-    height={14}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"
-    />
-  </svg>
-)
+  return text
+}
+
 const icPlus = (
   <svg
     fill="currentColor"
